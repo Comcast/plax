037c458a7309bc4a5037939270c7fa83d43edb21
Jamie Stephens <james_stephens@cable.comcast.com>
2021-10-28 14:34:23 +0000

    Move redaction code to separate files

--- dsl/ctx.go
+++ dsl/ctx.go
@@ -121,26 +121,6 @@ func (c *Ctx) SetLogLevel(level string) error {
 	return nil
 }
 
-// AddRedaction compiles the given string as a regular expression and
-// installs that regexp as a desired redaction in logging output.
-func (c *Ctx) AddRedaction(pat string) error {
-	r, err := regexp.Compile(pat)
-	if err != nil {
-		return err
-	}
-	c.Redactions[pat] = r
-	return nil
-}
-
-// WantsRedaction reports whether the parameter's value should be
-// redacted.
-//
-// Currently if a parameter starts with "X_" after ignoring special
-// characters, then the parameter's value should be redacted.
-func WantsRedaction(p string) bool {
-	return strings.HasPrefix(strings.Trim(p, "?!*"), "X_")
-}
-
 // bindingRedactions adds redaction patterns for values of binding
 // variables that start with X_.
 func (ctx *Ctx) BindingsRedactions(bs Bindings) error {
@@ -164,58 +144,6 @@ func (ctx *Ctx) BindingsRedactions(bs Bindings) error {
 		}
 	}
 	return nil
-}
-
-// Redact might replace part of s with <redacted> depending on the
-// given Regexp.
-//
-// If the Regexp has no groups, all substrings that match the Regexp
-// are redacted.
-//
-// For each named group with a name starting with "redact", that group
-// is redacted (for all matches).
-//
-// If there are groups but none has a name starting with "redact",
-// then the first matching (non-captured) group is redacted.
-func Redact(r *regexp.Regexp, s string) string {
-	replacement := "<redacted>"
-	if r.NumSubexp() == 0 {
-		return r.ReplaceAllString(s, replacement)
-	}
-
-	var acc string
-	for {
-		match := r.FindStringSubmatchIndex(s)
-		if match == nil {
-			acc += s
-			break
-		}
-		var (
-			redacted   = false
-			names      = r.SubexpNames()
-			last       = match[1]
-			start, end int
-		)
-		for i, name := range names {
-			// First one is anonymous everything group.
-			if strings.HasPrefix(name, "redact") {
-				redacted = true
-				start, end = match[2*i], match[2*i+1]
-				break
-			}
-		}
-
-		if !redacted {
-			// The first group will be redacted.
-			start, end = match[2], match[3]
-		}
-
-		acc += s[0:start] + replacement + s[end:last]
-
-		s = s[last:]
-	}
-
-	return acc
 }
 
 // Redactf calls c.Printf with any requested redactions with c.Redact
